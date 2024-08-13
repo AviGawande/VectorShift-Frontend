5ba4ed7d623e83230232aa52d397dd9ac9ea4a90
Abhishek  Gawande <2041024@gcoej.ac.in>
2024-08-13 13:52:27 +0530

    added-routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import { IntegrationForm } from './integration-form';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import SignIn from './components/Signin';   // Assuming you have a SignIn component
+import SignUp from './components/Signup';   // Assuming you have a SignUp component
+import User from './components/User';       // Assuming you have a User component
 
 function App() {
   return (
-    <div>
-      <IntegrationForm />
-    </div>
+    <BrowserRouter>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/user" element={<User />} />
+        </Routes>
+      </div>
+    </Router>
+    </BrowserRouter>
   );
 }
 
